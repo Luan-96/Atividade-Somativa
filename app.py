e7038f5eaed3336a95693ca8da2a70b0404767f7
Luan-96 <luanferreira1996@gmail.com>
2025-09-14 22:49:51 -0300

    Adicionando mais frases

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ quotes = [
     "A única maneira de fazer um ótimo trabalho é amar o que você faz. - Steve Jobs",
     "A persistência é o caminho do êxito. - Charles Chaplin",
     "O sucesso é a soma de pequenos esforços repetidos dia após dia. - Robert Collier",
-    "A imaginação é mais importante que o conhecimento. - Albert Einstein"
+    "A imaginação é mais importante que o conhecimento. - Albert Einstein",
+    "Se você quer viver uma vida feliz, amarre-se a um objetivo, não às pessoas ou às coisas. - Albert Einstein"
 ]
-
 @app.route("/")
 def home():
     return "<h1>API de Citações</h1><p>Visite /quote para uma citação aleatória.</p>"
